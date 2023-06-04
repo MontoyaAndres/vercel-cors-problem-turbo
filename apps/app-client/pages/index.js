71029a42f3e72b2c647790b09a39ec0384ce7c1b
MontoyaAndres <andresmontoyafcb@gmail.com>
2023-06-04 11:19:06 -0500

    test

--- apps/app-client/pages/index.js
+++ apps/app-client/pages/index.js
@@ -2,22 +2,22 @@ import { useEffect } from "react";
 
 const Index = () => {
   useEffect(() => {
-    const getReponse = async () => {
-      // configure here your vercel domain
-      const response = await fetch(
-        "https://vercel-cors-problem-turbo-app-api.vercel.app/api",
-        {
-          headers: { Accept: "application/json" },
-        }
-      ).then((r) => r.json());
-
-      console.log(response);
-    };
-
     getReponse();
   }, []);
 
-  return <h1>hi</h1>;
+  const getReponse = async () => {
+    // configure here your vercel domain
+    const response = await fetch(
+      "https://vercel-cors-problem-turbo-app-api.vercel.app/api",
+      {
+        headers: { Accept: "application/json" },
+      }
+    ).then((r) => r.json());
+
+    console.log(response);
+  };
+
+  return <h1 onClick={getReponse}>hi</h1>;
 };
 
 export default Index;
